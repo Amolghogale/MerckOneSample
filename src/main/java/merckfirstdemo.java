195931e437aa69015ca5c94f7e7e986db7ed0def
aghogale <aghogale@BNGLT-FH482Z2.global.sial.com>
2022-01-19 12:11:57 +0530

    first demo

---
+++ src/main/java/merckfirstdemo.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+public class merckfirstdemo extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	public static void main(String[] args) {
+		Server server = new Server(Integer.valueOf(System.getenv("PORT")));
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		Server server = new Server(Integer.valueOf(System.getenv("PORT")));
+		ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+		context.setContextPath("/");
+		server.setHandler(context);
+		context.addServlet(new ServletHolder(new merckfirstdemo()), "/*");
+		resp.getWriter().print("Welcome Merck to Heroku training !!!");
+	}
+}
